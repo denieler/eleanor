d4323a24d8a18275f519a4a6940ded4e66b2006d
Daniel Ostapenko <danielostapenko@gmail.com>
2017-11-05 19:39:27 +0100

    feat: add requesting Bittrex to retry tick data

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # eleanor
 Server for collecting trading data from Bittrex
+
+## Development notes
+
+To rise up redis docker container: 
+```
+docker run --name some-redis -d -p 6379:6379 redis
+```
---
+++ index.js
@@ -0,0 +1,45 @@
+const redis = require('redis')
+const Promise = require('bluebird')
+Promise.promisifyAll(redis.RedisClient.prototype)
+Promise.promisifyAll(redis.Multi.prototype)
+
+const bittrexApi = require('node.bittrex.api')
+const uuid = require('uuid/v1')
+
+const redisClient = redis.createClient()
+
+redisClient.on('error', function (err) {
+    console.log('Error ' + err)
+})
+
+function addCurrencyRecord (currency, tradingTick) {
+    const tickId = uuid()
+    const createDate = new Date().getTime()
+
+    redisClient.hmset('data:' + currency + ':' + tickId, {
+        bid: tradingTick.Bid,
+        ask: tradingTick.Ask,
+        last: tradingTick.Last
+    }, (err, obj) => {
+        if (err) {
+            console.log('Error adding currency record. Data:', obj, currency, tradingTick)
+        }
+    })
+
+    redisClient.zadd(['timeData', createDate, tickId], (err, obj) => {
+        if (err) {
+            console.log('Error adding filtering set of currency record. Data:', obj, createDate, currency, tradingTick)
+        }
+    })
+}
+
+const currency = 'BTC'
+setInterval(_ => {
+    bittrexApi.getticker({ market: 'USDT-' + currency }, (data, err) => {
+        if (data.success) {
+            addCurrencyRecord(currency, data.result)
+        } else {
+            console.log('getticker request failed. Error:', err, data)
+        }
+    })
+}, 1000)
